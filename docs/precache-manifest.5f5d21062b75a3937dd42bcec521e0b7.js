02a9368a9be8f5ae9f6a8723d09e4ad186ab4a99
Taskkill <johny95wil@gmail.com>
2021-01-16 13:15:14 +0100

    rebuild with #1 fixed

---
+++ docs/precache-manifest.5f5d21062b75a3937dd42bcec521e0b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce7478bacca17eef607d1ce4d493bd7",
+    "url": "/holo/index.html"
+  },
+  {
+    "revision": "7aee0d545a30ba4cb92b",
+    "url": "/holo/static/css/2.b507a811.chunk.css"
+  },
+  {
+    "revision": "6ddaf57abc288ed0a9e8",
+    "url": "/holo/static/css/main.fcb09c3a.chunk.css"
+  },
+  {
+    "revision": "7aee0d545a30ba4cb92b",
+    "url": "/holo/static/js/2.be6e62a6.chunk.js"
+  },
+  {
+    "revision": "2225c84c946de3dbee439e7e753521ae",
+    "url": "/holo/static/js/2.be6e62a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ddaf57abc288ed0a9e8",
+    "url": "/holo/static/js/main.fcd8a4e7.chunk.js"
+  },
+  {
+    "revision": "314050cf25b24cb95f1e",
+    "url": "/holo/static/js/runtime-main.02fdb955.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.206a6a3f45e16c38365ced871f0f2690.js"
+  "/holo/precache-manifest.5f5d21062b75a3937dd42bcec521e0b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/holo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
